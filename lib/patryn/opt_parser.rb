88e4909c333be9c9d41c2e87394b01f113d88754
David Saenz <david.saenz.tagarro@gmail.com>
2016-04-10 06:41:02 +0200

    Adds parser options

--- lib/patryn/opt_parser.rb
+++ lib/patryn/opt_parser.rb
@@ -1,10 +1,14 @@
-require 'opt_parser'
+require 'optparse'
 
 module Patryn
   # Responsible for parsing script options
   module OptParser
     def parse_options
-
+      def parse_options
+        @options = default_options
+        opt_parser.parse!
+      end
+			d=
     end
   end
 end
